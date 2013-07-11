ad5d5d61d43b632c8cfcd350b899c9278960cb91
Greg Michalec <greg@primate.net>
2013-07-10 18:55:15 -0700

    make update_all_data work better

--- backend/update_all_data.php
+++ backend/update_all_data.php
@@ -1,5 +1,11 @@
 <?php
-system("php download_influence_explorer_data.php") #Fill in nimsp_offices and nimsp_ballots 
-system("php fetch_nimsp_data.php") #Fill in nimsp_offices and nimsp_ballots 
-system("php fetch_sunlight_legislators.php") #Fill in legislators and legistlator_terms
-system("php fetch_candidate_images.php") #Fill in legislators and legistlator_terms
+if (! isset($argv[1])) { 
+	print "Usage: php update_all_data.php <download_influence_explorer_data_boolean>\n"; exit;
+}
+if ($argv[1]) { 
+	system("php download_influence_explorer_data.php 1"); #Fill in nimsp_offices and nimsp_ballots 
+}
+system("php fetch_nimsp_data.php"); #Fill in nimsp_offices and nimsp_ballots 
+system("php fetch_sunlight_legislators.php"); #Fill in legislators and legistlator_terms
+system("php fetch_candidate_images.php"); #Fill in legislators and legistlator_terms
+system("php import_state_dem_data.php"); #Fill in legislators and legistlator_terms
