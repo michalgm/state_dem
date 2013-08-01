8fed6b12c4baab9d28fc15760f4360dec94a2103
Stanley Jones <greg@primate.net>
2013-08-01 10:12:07 -0700

    Frontend database dump from 2013.08.01-10.12

--- backend/publish/db.sql
+++ backend/publish/db.sql
@@ -197,7 +197,7 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-08-01  9:34:29
+-- Dump completed on 2013-08-01 10:12:07
 -- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: oilchange
@@ -284,4 +284,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-08-01  9:34:30
+-- Dump completed on 2013-08-01 10:12:07
