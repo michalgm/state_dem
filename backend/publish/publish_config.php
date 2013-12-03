542408dee0501a02e13ed538a6989b68e12bcd81
Greg Michalec <greg@primate.net>
2013-12-02 17:33:53 -0800

    add reports table to list of published tables

--- backend/publish/publish_config.php
+++ backend/publish/publish_config.php
@@ -2,7 +2,7 @@
 $localdb="state_dem";
 $remotedb="priceofoil_states";
 $remote_staging_db="priceofoil_states_staging";
-$live_db_tables="contributions_dem legislators legislator_terms states years";
+$live_db_tables="contributions_dem legislators legislator_terms states years reports";
 #login="corpwatchapi@192.168.2.111";
 $login="priceofoil@priceofoil.electricembers.net";
 $port="21182";
